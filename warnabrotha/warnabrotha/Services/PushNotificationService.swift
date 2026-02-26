b084f7d4f5f4384a5485d8efccc1efbfc6fc4f71
Abhinav Tata <atata@ucdavis.edu>
2026-02-25 19:25:43 -0800

    time sensitive taps notifs

--- warnabrotha/warnabrotha/Services/PushNotificationService.swift
+++ warnabrotha/warnabrotha/Services/PushNotificationService.swift
@@ -17,7 +17,7 @@ class PushNotificationService {
     func requestPermissionAndRegister() async -> Bool {
         let center = UNUserNotificationCenter.current()
         do {
-            let granted = try await center.requestAuthorization(options: [.alert, .sound, .badge])
+            let granted = try await center.requestAuthorization(options: [.alert, .sound, .badge, .timeSensitive])
             if granted {
                 UIApplication.shared.registerForRemoteNotifications()
             }
